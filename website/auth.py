dbc7722b62b69b17757cd7a0f58746d5e91a47e3
AlexNordenhog <alex.nordenhog@gmail.com>
2024-02-20 13:47:23 +0100

    change of framework

---
+++ website/auth.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, render_template
+
+auth = Blueprint("auth", __name__)
+
+
+@auth.route("/login")
+def login():
+    return render_template("login.html", text="banan")
+
+
+@auth.route("/logout")
+def logout():
+    return "<p>logout</p>"
+
+
+@auth.route("/sign-up")
+def sign_up():
+    return render_template("sign_up.html")
