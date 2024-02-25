a8a4f55579e67610a058b0b8f53e7f4aa97ab312
simonflisberg <sifl21@student.bth.se>
2024-02-25 15:24:56 +0100

    Search funktionen, ska implementeras med backend

--- website/search.py
+++ website/search.py
@@ -1,12 +1,44 @@
 import difflib
 
+def search(university_data, query, university=None, subject=None, course=None):
+    '''
+    Filters university_data based on university, subject, and course selections, and then
+    performs a keyword search within that filtered list if a query is provided.
+    
+    Parameters:
+    - university_data: A nested dictionary with structure {'University': {'Subject': ['Course', ...]}}
+    - query: Search keyword provided by the user. Can be empty, meaning no keyword search.
+    - university: Selected university.
+    - subject: Selected subject.
+    - course: Selected course.
 
-def search(keywords, search_keyword):
+    Returns a list of matching courses based on the search criteria.
     '''
-    Takes a list of keywords and a search keyword as parameters and returns a list of matching keywords from the keywords list.
-    This search function is case-insenstive and allows for partial matches.
-    '''
-    lower_keywords = [keyword.lower() for keyword in keywords]
-    close_matches = difflib.get_close_matches(search_keyword.lower(), lower_keywords, n=len(keywords), cutoff=0.5)
-    matching_keywords = [keywords[lower_keywords.index(match)] for match in close_matches]
-    return matching_keywords
+    filtered_courses = []
+
+    if university in university_data:
+        uni_data = university_data[university]
+        if subject in uni_data:
+            subj_data = uni_data[subject]
+            if course in subj_data:
+                filtered_courses = [c for c in subj_data if course.lower() in c.lower()]
+            else:
+                filtered_courses = subj_data
+        else:
+            for subj_courses in uni_data.values():
+                filtered_courses.extend(subj_courses)
+    else:
+        for uni_subjects in university_data.values():
+            for subj_courses in uni_subjects.values():
+                filtered_courses.extend(subj_courses)
+
+    matching_courses = []
+    if query:
+        lower_filtered_courses = [c.lower() for c in filtered_courses]
+        n_matches = max(1, len(filtered_courses))
+        close_matches = difflib.get_close_matches(query.lower(), lower_filtered_courses, n=n_matches, cutoff=0.5)
+        matching_courses = [filtered_courses[lower_filtered_courses.index(match)] for match in close_matches]
+    else:
+        matching_courses = filtered_courses
+
+    return matching_courses
--- website/views.py
+++ website/views.py
@@ -1,20 +1,39 @@
 from flask import Blueprint, request, render_template
 from .search import search
+import json
 
 views = Blueprint("views", __name__)
 
+#Temporärt för att prova sökfunktionen, ta bort senare.
+course_list = ['Analys 1', 'Analys 2', 'Matematisk Problemlösning', 'Mikroekonomi', 'Flervariabelanalys', 'Datorstöd för Ingenjörer', 'Programvaruintensiv Produktutveckling', 'Objektorienterad Design', 'Grunderna i Industriell Ekonomi']
+universities = ['Blekinge Institute of Technology', 'Chalmers Institute of Technology']
+university_data = {
+    'Blekinge Institute of Technology': {
+      'Mathematics': ['Analys 1', 'Analys 2', 'Matematisk Problemlösning', 'Flervariabelanalys'],
+      'Economics': ['Mikroekonomi', 'Grunderna i Industriell Ekonomi'],
+      'CAD': ['Datorstöd för Ingenjörer'],
+      'IT': ['Programvaruintensiv Produktutveckling', 'Objektorienterad Design']
+    },
+    'Chalmers Institute of Technology': {
+      'Mathematics': ['Inledande Matematisk Analys']
+    }
+  }
 
 @views.route("/")
 def home():
-    return render_template("home.html")
-
-#Temporär lista för att prova sökfunktionen, ta bort senare.
-dummy_list = ['Analys 1', 'Analys 2', 'Matematisk Problemlösning', 'Mikroekonomi', 'Flervariabelanalys', 'Datorstöd för Ingenjörer', 'Programvaruintensiv Produktutveckling', 'Objektorienterad Design', 'Grunderna i Industriell Ekonomi']
+    return render_template("home.html",
+                           universities=universities,
+                           university_data=json.dumps(university_data))
+                           
 
 @views.route("/search")
 def search_results():
+    university = request.args.get('university')
+    subject = request.args.get('subject')
+    course = request.args.get('course')
     query = request.args.get('query', '')
-    results = search(dummy_list, query)  
+    
+    results = search(university_data, query, university, subject, course)  
     return render_template('search_results.html', query=query, results=results)
 
 
