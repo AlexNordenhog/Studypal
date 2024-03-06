0cf4a6cf2d5d520ca9271550506d2d863881f94c
Hampus <sodergren.hampus@gmail.com>
2024-03-06 13:20:01 +0100

    user wip

--- website/static/auth.js
+++ website/static/auth.js
@@ -1,17 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { GoogleAuthProvider, getAuth, signInWithPopup, onAuthStateChanged, updateProfile } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 import { getDatabase, ref, get, child, set } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyAjihd9ILzcVlPzCWrr4SLVJCOw6I1nzAc",
-    authDomain: "studypal-8a379.firebaseapp.com",
-    databaseURL: "https://studypal-8a379-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "studypal-8a379",
-    storageBucket: "studypal-8a379.appspot.com",
-    messagingSenderId: "475383216328",
-    appId: "1:475383216328:web:dcebc7bfd95236ebd7cdfd",
-    measurementId: "G-68F0PLPVS6"
-};
+import firebaseConfig from "./firebase-cfg.js";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
--- website/static/index.js
+++ website/static/index.js
@@ -1,17 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { GoogleAuthProvider, getAuth, signInWithPopup, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyAjihd9ILzcVlPzCWrr4SLVJCOw6I1nzAc",
-    authDomain: "studypal-8a379.firebaseapp.com",
-    databaseURL: "https://studypal-8a379-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "studypal-8a379",
-    storageBucket: "studypal-8a379.appspot.com",
-    messagingSenderId: "475383216328",
-    appId: "1:475383216328:web:dcebc7bfd95236ebd7cdfd",
-    measurementId: "G-68F0PLPVS6"
-    };
+import firebaseConfig from "./firebase-cfg.js";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
